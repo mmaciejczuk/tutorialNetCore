1c17c20ad74b443bdfec489ce0ca03ba5842cc65
Mariusz Maciejczuk <mariusz.maciejczuk@intive.com>
2018-03-15 16:11:56 +0100

    final changes to working controller

---
+++ src/Evento.Api/Domain/Entity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Evento.Api.Domain
+{
+    public class Entity
+    {
+        public Guid id { get; set; }
+    }
+}
--- src/Evento.Infrastructure/Services/EventService.cs
+++ src/Evento.Infrastructure/Services/EventService.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Evento.Core.Repositories;
 using Evento.Infrastructure.DTO;
 
@@ -9,23 +11,30 @@ namespace Evento.Infrastructure.Services
     public class EventService : IEventService
     {
         private readonly IEventRepository _eventRepository;
-        public EventService(IEventRepository eventRepository)
+        private readonly IMapper _mapper;
+        public EventService(IEventRepository eventRepository, IMapper mapper)
         {
             _eventRepository = eventRepository;
+            _mapper = mapper;
         }
         public async Task<EventDto> GetAsync(Guid id)
         {
-            var @event = await
+            var @event = await _eventRepository.GetAsync(id);
+            return _mapper.Map<EventDto>(@event);
         }
 
         public async Task<EventDto> GetAsync(string name)
         {
-            throw new NotImplementedException();
+            var @event = await _eventRepository.GetAsync(name);
+            return _mapper.Map<EventDto>(@name);
         }
+
         public async Task<IEnumerable<EventDto>> BrowseAsync(string name = null)
         {
-            throw new NotImplementedException();
+            var events = await _eventRepository.BrowseAsync(name);
+            return _mapper.Map<IEnumerable<EventDto>>(events);
         } 
+
         public async Task CreateAsync(Guid id, string name, string description, DateTime startDate, DateTime endDate)
         {
             throw new NotImplementedException();
